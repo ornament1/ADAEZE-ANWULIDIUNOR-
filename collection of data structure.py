67489faafaa90146dbee31860865a0d0c10ab341
Adaeze <aanwulidiunor@gmail.com>
2024-12-04 03:54:18 +0100

    my first upload

---
+++ collection of data structure.py
@@ -0,0 +1,4 @@
+numbers = [23, 44, 55, 60, 24, 25, 26, 55, 44, 9]
+
+for i in range(len(numbers)-1,-1,-1):
+    print(numbers[i])
---
+++ dictionary.py
@@ -0,0 +1,9 @@
+igbo_dictionary = {
+    'bia': 'come',
+
+    'gawa': 'go',
+
+    'otu': 'one',
+}
+word = input('Enter a igbo word')
+print(igbo_dictionary[word])
